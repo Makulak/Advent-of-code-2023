a889195238a469cac49405467f87ee4c55d51bb1
Jonasz Makulak <j.makulak@gmail.com>
2023-12-01 15:33:48 +0100

    Day 1

---
+++ Advent-of-code-2023/1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+string[] text = File.ReadAllLines(@"../../../input.txt");
+
+int sum = 0;
+
+var stringNumbersList = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+foreach (var line in text)
+{
+    var first = FindFirst(line);
+    var last = FindLast(line);
+
+    sum += first * 10 + last;
+}
+
+int FindFirst(string line)
+{
+    int minIdx = 0;
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (int.TryParse(line[i].ToString(), out var result))
+            return result;
+
+        var stringToCompare = new String(line.Take(i + 1).ToArray());
+        var map = stringNumbersList.Select(stringToCompare.Contains).ToList();
+
+        if (map.Any(x => x == true))
+            return map.IndexOf(true) + 1;
+    }
+
+    throw new Exception("XD");
+}
+
+int FindLast(string line)
+{
+    int minIdx = 0;
+    for (int i = line.Length - 1; i >= 0; i--)
+    {
+        if (int.TryParse(line[i].ToString(), out var result))
+            return result;
+
+        var stringToCompare = new String(line.Skip(i).ToArray());
+        var map = stringNumbersList.Select(stringToCompare.Contains).ToList();
+
+        if (map.Any(x => x == true))
+            return map.IndexOf(true) + 1;
+    }
+
+    throw new Exception("XD");
+}
+
+Console.WriteLine(sum);
