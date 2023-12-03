ea3081db06eba3d56a264a7f02450f0b9c73078f
Jonasz Makulak <j.makulak@gmail.com>
2023-12-03 23:01:58 +0100

    Day 2 and Day 3 pt 1

---
+++ Advent-of-code-2023/2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+
+string[] text = File.ReadAllLines(@"../../../input.txt");
+
+var maxGreen = 13;
+var maxBlue = 14;
+var maxRed = 12;
+
+var possibleGamesIdsCount = 0;
+var power = 0;
+
+foreach (string line in text)
+{
+    var isPossible = true;
+    var lineMatch = Regex.Match(line, @"Game ([0-9]+): (.+)");
+
+    var gameId = lineMatch.Groups[1].Value;
+    var listOfResults = lineMatch.Groups[2].Value.Split(";");
+
+    var minRed = 0; 
+    var minGreen = 0;
+    var minBlue = 0; 
+
+    foreach (var result in listOfResults)
+    {
+        var cubes = result.Split(",");
+
+        foreach (var cube in cubes)
+        {
+            var cubeMatch = Regex.Match(cube, @"([0-9]+) (red|green|blue)");
+
+            var cubesCount = int.Parse(cubeMatch.Groups[1].Value);
+            var cubeColor = cubeMatch.Groups[2].Value;
+
+            if (cubeColor == "red" && minRed < cubesCount)
+                minRed = cubesCount;
+            if (cubeColor == "green" && minGreen < cubesCount)
+                minGreen = cubesCount;
+            if (cubeColor == "blue" && minBlue < cubesCount)
+                minBlue = cubesCount;
+
+            //if (!IsPossible(int.Parse(cubesCount), cubeColor))
+            //{
+            //    isPossible = false;
+            //    break;
+            //}
+        }
+        //if (!isPossible)
+            //break;
+    }
+    power += minRed * minGreen * minBlue;
+
+    //if (isPossible)
+    //    possibleGamesIdsCount += int.Parse(gameId);
+}
+
+//Console.WriteLine(possibleGamesIdsCount);
+Console.WriteLine(power);
+
+bool IsPossible(int cubesCount, string cubeColor)
+{
+    if (cubeColor == "red")
+        return maxRed >= cubesCount;
+    if (cubeColor == "green")
+        return maxGreen >= cubesCount;
+    if (cubeColor == "blue")
+        return maxBlue >= cubesCount;
+    else
+        throw new Exception("Impossible color");
+}
---
+++ Advent-of-code-2023/3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+string[] text = File.ReadAllLines(@"../../../input.txt");
+
+var sum = 0;
+
+for (int y = 0; y < text.Length; y++)
+{
+    var line = text[y];
+
+    var numbers = Regex.Matches(line, @"\d+");
+
+    foreach (Match number in numbers)
+    {
+        var numberStartX = Math.Max(0, number.Index - 1);
+        var numberEndX = Math.Min(line.Length - 1, numberStartX + number.Length + 1);
+
+        var numberStartY = Math.Max(0, y - 1);
+        var numberEndY = Math.Min(text.Length - 1, y + 1);
+
+        var length = number.Index == 0 ? numberEndX - numberStartX : numberEndX - numberStartX + 1;
+
+        var x = text.Select(x => x.Substring(numberStartX, length))
+            .ToList();
+
+        var test = new String(text.Select(x => x.Substring(numberStartX, length))
+            .Skip(numberStartY)
+            .Take(numberEndY - numberStartY + 1)
+            .SelectMany(x => x).ToArray());
+
+        if (Regex.IsMatch(test, @"[^a-zA-Z0-9.]"))
+            sum += int.Parse(number.Value);
+    }
+}
+
+Console.WriteLine(sum);
