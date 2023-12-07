7283ab8fdbf048bf73a010a750eb29d09afb30f6
Jonasz Makulak <j.makulak@gmail.com>
2023-12-07 11:23:51 +0100

    Day 6, Day 7 pt 1

---
+++ Advent-of-code-2023/6/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] text = File.ReadAllLines(@"../../../input.txt");
+
+//List<int> Times = text.First().Split(":").Last().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
+//List<int> Distaces = text.Last().Split(":").Last().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
+
+List<long> Times = new List<long>() { long.Parse(text.First().Split(":").Last().Replace(" ", "")) };
+List<long> Distaces = new List<long>() { long.Parse(text.Last().Split(":").Last().Replace(" ", "")) };
+
+var result = 1;
+
+for(int i = 0; i < Times.Count; i++)
+{
+    var raceTime = Times[i];
+    var recordDistance = Distaces[i];
+
+    var counter = 0;
+
+    for(int j = 1; j < raceTime; j++)
+    {
+        var distance = j * (raceTime - j);
+
+        if (distance > recordDistance)
+            counter++;
+    }
+
+    result *= counter;
+}
+
+Console.WriteLine(result);
---
+++ Advent-of-code-2023/7/Program.cs
@@ -0,0 +1,118 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var game = new CamelGame();
+        string[] text = File.ReadAllLines(@"../../../input.txt");
+
+        foreach (string line in text)
+        {
+            var cards = line.Split(" ").First();
+            var bidValue = int.Parse(line.Split(" ").Last());
+
+            game.Hands.Add(new Hand(cards, bidValue));
+        }
+
+        Console.WriteLine(game.GetResult());
+    }
+}
+
+public enum HandType
+{
+    HighCard,
+    OnePair,
+    TwoPairs,
+    Three,
+    Full,
+    Four,
+    Five
+}
+
+public class CamelGame
+{
+    public List<Hand> Hands { get; set; }
+    public CamelGame()
+    {
+        Hands = new();
+    }
+
+    public int GetResult()
+    {
+        var ordered = Hands.Order().ToList();
+        return Hands
+            .Order()
+            .Select((x, idx) => new { Rank = idx + 1, Hand = x })
+            .Aggregate(0, (sum, element) => sum += element.Rank * element.Hand.BetValue);
+    }
+}
+
+public class Hand : IComparable<Hand>
+{
+    public string Cards { get; set; }
+    public List<int> CardValues { get; set; }
+    public int BetValue { get; set; }
+
+    public Hand(string cards, int betValue)
+    {
+        Cards = cards;
+        BetValue = betValue;
+
+        CardValues = cards.Select(x =>
+        {
+            var map = new Dictionary<string, int>()
+            {
+                { "T", 10 },
+                { "J", 11 },
+                { "Q", 12 },
+                { "K", 13 },
+                { "A", 14 },
+            };
+            if (int.TryParse(x.ToString(), out var num))
+                return num;
+
+            return map[x.ToString()];
+        }).ToList();
+    }
+
+    public HandType GetHandType()
+    {
+        var groupped = Cards.GroupBy(x => x);
+        if (groupped.Count() == 1)
+            return HandType.Five;
+        else if (groupped.Any(x => x.Count() == 4))
+            return HandType.Four;
+        else if (groupped.Any(x => x.Count() == 3))
+        {
+            if (groupped.Any(x => x.Count() == 2))
+                return HandType.Full;
+
+            return HandType.Three;
+        }
+        else if (groupped.Where(x => x.Count() == 2).Count() == 2)
+            return HandType.TwoPairs;
+        else if (groupped.Where(x => x.Count() == 2).Count() == 1)
+            return HandType.OnePair;
+
+        return HandType.HighCard;
+    }
+
+    public int CompareTo(Hand? other)
+    {
+        var currentHandType = GetHandType();
+        var otherHandType = other.GetHandType();
+
+        if (currentHandType == otherHandType)
+        {
+            for (int i = 0; i < CardValues.Count(); i++)
+            {
+                var compareResult = CardValues[i].CompareTo(other.CardValues[i]);
+
+                if(compareResult != 0)
+                    return compareResult;
+            }
+            return 0;
+        }
+
+        return currentHandType.CompareTo(otherHandType);
+    }
+}
