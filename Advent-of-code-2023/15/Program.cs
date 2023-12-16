0cd4eb5129deb2167cd923f97ea3811e6f240fdd
Jonasz Makulak <j.makulak@gmail.com>
2023-12-16 11:33:26 +0100

    Day 15 pt1

---
+++ Advent-of-code-2023/15/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text;
+
+string text = File.ReadAllLines(@"../../../input.txt").Single();
+List<string> list = text.Split(',').ToList();
+int sum = 0;
+
+foreach (string step in list)
+{
+    sum += CalculateHash(step);
+}
+
+Console.WriteLine(sum);
+
+int CalculateHash(string step)
+{
+    int current = 0;
+
+    foreach(var c in Encoding.ASCII.GetBytes(step))
+    {
+        current += (int)c;
+        current *= 17;
+        current = current % 256;
+    }
+
+    return current;
+}
